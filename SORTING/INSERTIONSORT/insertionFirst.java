43289abc5a37f29b6ae3d88843593ab701299b51
lucky-mehta <mehtalucky2005@gmail.com>
2026-06-12 17:16:06 +0530

    second commit

---
+++ SORTING/INSERTIONSORT/insertionFirst.java
@@ -0,0 +1,24 @@
+package SORTING.INSERTIONSORT;
+
+public class insertionFirst {
+  public static void printArray(int arr[]) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args) {
+    int arr[] = { 12, 3, 45, 96, 8, 14 };
+    for (int i = 1; i < arr.length; i++) {
+      int current = arr[i];
+      int j = i - 1;
+      while (j >= 0 && current < arr[j]) {
+        arr[j + 1] = arr[j];
+        j--;
+      }
+      arr[j + 1] = current;
+    }
+    printArray(arr);
+  }
+}
