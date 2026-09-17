acc2fd2ced0054d56ecfa5ce3a3063f6a73d88e0
lucky-mehta <mehtalucky2005@gmail.com>
2026-09-17 17:30:12 +0530

    stack commit

--- STACK/infix1.java
+++ STACK/infix1.java
@@ -2,9 +2,9 @@ package STACK;
 
 import java.util.Stack;
 
-public class infix {
+public class infix1 {
   public static void main(String[] args) {
-    String str = "9-5+3*4/6";
+    String str = "9-(5+3)*4/6";
     Stack<Integer> val = new Stack<>();
     Stack<Character> op = new Stack<>();
     for (int i = 0; i < str.length(); i++) {
@@ -12,8 +12,24 @@ public class infix {
       int ascii = (int) ch;
       if (ascii >= 48 && ascii <= 57) {
         val.push(ascii - 48);
-      } else if (op.size() == 0) {
+      } else if (op.size() == 0 || ch == '(' || op.peek() == '(') {
         op.push(ch);
+      } else if (ch == ')') {
+        while (op.peek() != '(') {
+          // work
+          int v2 = val.pop();
+          int v1 = val.pop();
+          if (op.peek() == '-')
+            val.push(v1 - v2);
+          if (op.peek() == '+')
+            val.push(v1 + v2);
+          if (op.peek() == '*')
+            val.push(v1 * v2);
+          if (op.peek() == '/')
+            val.push(v1 / v2);
+          op.pop();
+        }
+        op.pop(); // '(' ise remove kr diya
       } else {
         if (ch == '+' || ch == '-') {
           // work
@@ -30,7 +46,9 @@ public class infix {
           op.pop();
           // push
           op.push(ch);
-        } else
+        }
+
+        else
           op.push(ch);
       }
     }
