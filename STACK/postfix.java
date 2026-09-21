e7c8577092da31e0972f8b2acea9ddd4605dc438
lucky-mehta <mehtalucky2005@gmail.com>
2026-09-21 06:24:48 +0530

    Stack commit

---
+++ STACK/postfix.java
@@ -0,0 +1,60 @@
+
+// infix to postfix
+
+package STACK;
+
+import java.util.Stack;
+
+public class postfix {
+  public static void main(String[] args) {
+    String infix = "9-(5+3)*4/6";
+    System.out.println(infix);
+    Stack<String> val = new Stack<>();
+    Stack<Character> op = new Stack<>();
+    for (int i = 0; i < infix.length(); i++) {
+      char ch = infix.charAt(i);
+      int ascii = (int) ch;
+      // If digit
+      if (ascii >= 48 && ascii <= 57) {
+        String s = "" + ch;
+        val.push(s);
+      } else if (op.size() == 0 || ch == '(' || op.peek() == '(') {
+        op.push(ch);
+      } else if (ch == ')') {
+        while (op.peek() != '(') {
+          String v2 = val.pop();
+          String v1 = val.pop();
+          char o = op.pop();
+          String t = v1 + v2 + o;
+          val.push(t);
+        }
+        op.pop(); // remove '('
+      } else if (ch == '+' || ch == '-') {
+        String v2 = val.pop();
+        String v1 = val.pop();
+        char o = op.pop();
+        String t = o + v1 + v2;
+        val.push(t);
+        op.push(ch);
+      } else if (ch == '*' || ch == '/') {
+        if (op.peek() == '*' || op.peek() == '/') {
+          String v2 = val.pop();
+          String v1 = val.pop();
+          char o = op.pop();
+          String t = v1 + v2 + o;
+          val.push(t);
+        }
+        op.push(ch);
+      }
+    }
+    while (op.size() > 0) {
+      String v2 = val.pop();
+      String v1 = val.pop();
+      char o = op.pop();
+      String t = v1 + v2 + o;
+      val.push(t);
+    }
+    String postfix = val.pop();
+    System.out.println(postfix);
+  }
+}
